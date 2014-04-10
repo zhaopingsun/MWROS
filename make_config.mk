bb0079368cfaa395205c2d5374d6688198e71de3
roland sun rolandsun <roland sun rolandsun@gmail.com>
2014-04-10 14:07:55 +0800

    update for rhel6

--- make_config.mk
+++ make_config.mk
@@ -1,4 +1,5 @@
-
+GENE_CV_OBJ = generic_basedata_cv.o NameIndex.o genericTask.o \
+	 geneCodeData.o   generic_product_cv.o generic_dump.o
 CXXFLAGS= -Wall -Wno-sign-compare -ggdb -g3 -fdata-sections -ffunction-sections -fopenmp
 INCLUDES += -I ../lib
 %.o: %.cpp
