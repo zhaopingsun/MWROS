9dada4674e3f594c540dffe4e38ae61d97558db9
roland sun rolandsun <roland sun rolandsun@gmail.com>
2016-06-17 10:25:25 +0800

    add comment

--- njuqc/nju_qc.h
+++ njuqc/nju_qc.h
@@ -3,10 +3,11 @@
 #include "generic_basedata_cv.h"
 #define QC_OK 0
 //quaulity control for one cut data
+//caller should process vertical calbration data 
 //return QC_OK for succeed,otherwise define error code 
 int basedata_cut_qc(const  geneCutConfig &cut_config,const cvRadial &rawRadials,cvRadial &qcRadials);
 //select proper data from volume scan data and create rain rate from it as a PPI product
 //beam blockage should be processed ,HSR or similar strategy should be used
-//we just create RR from elevation 1.5 fro demo
+//we just create RR from elevation 1.5 for demo
 int basedata2rr(const basedataImage &bdi, struct PPIProdImage & pdi);
 #endif
